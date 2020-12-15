70147e06a83929b5680543ade33c24355274c66e
William Lam <william.lam.nz@gmail.com>
2020-12-15 22:11:44 +1300

    Add solution for day 15

---
+++ 2020/15/a.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+with open("input") as f:
+    nums = [int(n) for n in f.readline().split(",")]
+
+mem = {}
+for i, n in enumerate(nums, 1):
+    mem[n] = i
+
+i = len(nums) + 1
+last = nums[-1]
+while True:
+    spoken = 0
+    if last in mem:
+        spoken = (i - 1) - mem[last]
+
+    mem[last] = (i - 1)
+    last = spoken
+
+    if i == 2020:
+        print(last)
+        break
+
+    i += 1
