a8661de77b35976ac2bdd371d7c9b0b5f47ec81b
William Lam <william.lam.nz@gmail.com>
2020-12-19 15:50:45 +1300

    Add solution for day 18

---
+++ 2020/18/b.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+
+total = 0
+with open("input") as f:
+    for line in f:
+        s = []
+        q = []
+
+        tokens = line.strip().replace("(", "( ").replace(")", " )").split(" ")
+
+        for token in tokens:
+            if token == "*": 
+                if s and s[-1] != "(":
+                    q.append(s.pop())
+                s.append(token)
+            elif token == "+":
+                if s and s[-1] != "(" and s[-1] == "+":
+                    q.append(s.pop())
+                s.append(token)
+            elif token == "(":
+                s.append(token)
+            elif token == ")":
+                while True:
+                    op = s.pop()
+                    if op == "(":
+                        break
+                    q.append(op)
+            else:
+                q.append(int(token))
+
+        while s:
+            q.append(s.pop())
+
+        while q:
+            token = q.pop(0)
+
+            if token == "+":
+                operand1 = s.pop()
+                operand2 = s.pop()
+                
+                s.append(operand1 + operand2)
+            elif token == "*":
+                operand1 = s.pop()
+                operand2 = s.pop()
+                
+                s.append(operand1 * operand2)
+            else:
+                s.append(token)
+
+        total += s.pop()
+
+print(total)
