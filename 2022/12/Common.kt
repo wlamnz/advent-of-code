fd8f0f88e9b05c0d487a951beb198e19f72ef63b
William Lam <william.lam.nz@gmail.com>
2022-12-12 23:52:10 +1300

    Add solution for day 12 (2022)

---
+++ 2022/12/Common.kt
@@ -0,0 +1,3 @@
+package `12`
+
+class State(val r: Int, val c: Int, val height: Int, val id: Char, val steps: Int)
---
+++ 2022/12/b.kt
@@ -0,0 +1,50 @@
+package `12`
+
+import java.util.PriorityQueue
+
+fun main() {
+    val lines = object {}.javaClass.getResourceAsStream("input").bufferedReader().readLines()
+    val pq = PriorityQueue<State> { s1, s2 -> s1.steps - s2.steps }
+
+    for (r in lines.indices) {
+        for (c in lines[r].indices) {
+            if (lines[r][c] == 'S' || lines[r][c] == 'a') {
+                pq.add(State(r, c, 0, 'S', 0))
+            }
+        }
+    }
+
+    val visited = Array(lines.size) { BooleanArray(lines[0].length) }
+    val dirs = listOf(Pair(1, 0), Pair(0, 1), Pair(-1, 0), Pair(0, -1))
+
+    while (!pq.isEmpty()) {
+        val s = pq.poll()
+
+        if (s.id == 'E') {
+            println(s.steps)
+            break
+        }
+
+        if (!visited[s.r][s.c]) {
+            visited[s.r][s.c] = true
+
+            for (dir in dirs) {
+                val rr = s.r + dir.first
+                val cc = s.c + dir.second
+
+                if (rr >= 0 && rr < lines.size && cc >= 0 && cc < lines[0].length) {
+                    val c = lines[rr][cc]
+                    val h = when (lines[rr][cc]) {
+                        'S' -> 0
+                        'E' -> 25
+                        else -> c - 'a'
+                    }
+
+                    if (h <= s.height || s.height + 1 == h) {
+                        pq.add(State(rr, cc, h, c, s.steps + 1))
+                    }
+                }
+            }
+        }
+    }
+}
